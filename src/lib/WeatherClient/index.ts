1279eeffe39be69f99fab1dc5ca36f6c9d6ac36c
usaama ansari <usaama.ansari@massiveanalytic.com>
2023-03-27 01:45:45 +0530

    feat: integrate node-fetch with weather client

--- src/lib/WeatherClient/index.ts
+++ src/lib/WeatherClient/index.ts
@@ -1,10 +1,11 @@
-import request, { Response } from "request";
+import fetch, { Response } from "node-fetch";
 import { ICacheClient } from "@Lib/CacheClient/ICacheClient";
 import { IWeatherClient } from "./IWeatherClient";
 import { WEATHER_API_KEY, WEATHER_API_ENDPOINT } from "@Common/config";
 import { Result } from "@Common/logic";
 import { GenericObject } from "@Common/types";
 import { ApplicationError } from "@Common/errors";
+
 
 const CURRENT_WEATHER_ENDPOINT = `${WEATHER_API_ENDPOINT}/data/2.5/weather?`;
 const FIVE_DAYS_FORECAST_ENDPOINT = `${WEATHER_API_ENDPOINT}/data/2.5/forecast?`;
@@ -17,38 +18,25 @@ export class WeatherClient implements IWeatherClient {
   }
 
   async getCurrentWeather(cityName: string) {
-    return new Promise<Result<GenericObject | ApplicationError>>((resolve) => {
       const endpoint = `${CURRENT_WEATHER_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
       try {
-        request
-          .get(endpoint)
-          .on("response", (response: Response) => {
-            resolve(Result.ok(response.body));
-          })
-          .on("error", (error) => {
-            resolve(Result.fail(new ApplicationError(error.message, 502)));
-          });
+        const response = await fetch(endpoint);
+        const currentWeather = await response.json();
+        return Result.ok(currentWeather);
       } catch (err) {
-        resolve(Result.fail(new ApplicationError(err.message, 502)));
+        return Result.fail(new ApplicationError(err.message, 502))
       }
-    });
   }
 
   async getFiveDaysWeatherForecast(cityName: string) {
-    return new Promise<Result<GenericObject | ApplicationError>>((resolve) => {
-      const endpoint = `${FIVE_DAYS_FORECAST_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
-      try {
-        request
-          .get(endpoint)
-          .on("response", (response: Response) => {
-            resolve(Result.ok(response.body));
-          })
-          .on("error", (error) => {
-            resolve(Result.fail(new ApplicationError(error.message, 502)));
-          });
+    const endpoint = `${FIVE_DAYS_FORECAST_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
+    try {
+        const response = await fetch(endpoint);
+        const forecastData = await response.json();
+        return Result.ok(forecastData);
       } catch (err) {
-        resolve(Result.fail(new ApplicationError(err.message, 502)));
+        return Result.fail(new ApplicationError(err.message, 502))
       }
-    });
+  
   }
 }
