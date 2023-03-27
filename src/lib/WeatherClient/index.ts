b442982924087dec7034f55e540d6c744d687d0c
usaama ansari <usaama.ansari@massiveanalytic.com>
2023-03-27 15:57:27 +0530

    refactor(services): move cache client to weather service from weather client

--- src/lib/WeatherClient/index.ts
+++ src/lib/WeatherClient/index.ts
@@ -1,13 +1,12 @@
 import "reflect-metadata";
 import fetch from "node-fetch";
 import { ICacheClient } from "@Lib/CacheClient/ICacheClient";
+import { injectable } from "inversify";
 import { IWeatherClient } from "./IWeatherClient";
 import { WEATHER_API_KEY, WEATHER_API_ENDPOINT } from "@Common/config";
 import { Result } from "@Common/logic";
 import { GenericObject } from "@Common/types";
 import { ApplicationError } from "@Common/errorUtils";
-import { inject, injectable } from "inversify";
-import { IOC_TYPES } from "@Common/constants";
 import { HttpStatusCodes } from "@Common/HttpStatusCodes";
 
 const CURRENT_WEATHER_ENDPOINT = `${WEATHER_API_ENDPOINT}/data/2.5/weather?`;
@@ -15,57 +14,29 @@ const FIVE_DAYS_FORECAST_ENDPOINT = `${WEATHER_API_ENDPOINT}/data/2.5/forecast?`
 
 @injectable()
 export class WeatherClient implements IWeatherClient {
-  private cacheClient: ICacheClient;
-
-  constructor(@inject(IOC_TYPES.CacheClient) cacheClient: ICacheClient) {
-    this.cacheClient = cacheClient;
-  }
-
   async getCurrentWeather(cityName: string) {
-    const endpoint = `${CURRENT_WEATHER_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
     try {
-      /** First check if current weather data is in cache and resturn from cache */
-      const cachedDataResult = await this.cacheClient.getItem(
-        `${cityName}:current`
-      );
-
-      if (cachedDataResult.isSuccess) {
-        return Result.ok<GenericObject>(
-          cachedDataResult.getValue() as GenericObject
-        );
-      }
-
-      /** If not found in cache the make api call to openweather and add to cache and return data */
+      const endpoint = `${CURRENT_WEATHER_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
       const response = await fetch(endpoint);
       const currentWeather = await response.json();
-      await this.cacheClient.addItem(`${cityName}:current`, currentWeather);
       return Result.ok<GenericObject>(currentWeather);
     } catch (err) {
-      return Result.fail(new ApplicationError(err.message, HttpStatusCodes.BAD_REQUEST));
+      return Result.fail(
+        new ApplicationError(err.message, HttpStatusCodes.BAD_REQUEST)
+      );
     }
   }
 
   async getFiveDaysWeatherForecast(cityName: string) {
-    const endpoint = `${FIVE_DAYS_FORECAST_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
     try {
-      /** First check if 5 days weather forecast data is in cache and resturn from cache */
-      const cachedDataResult = await this.cacheClient.getItem(
-        `${cityName}:forecast`
-      );
-
-      if (cachedDataResult.isSuccess) {
-        return Result.ok<GenericObject>(
-          cachedDataResult.getValue() as GenericObject
-        );
-      }
-
-      /** If not found in cache the make api call to openweather and add to cache and return data */
+      const endpoint = `${FIVE_DAYS_FORECAST_ENDPOINT}q=${cityName}&appid=${WEATHER_API_KEY}`;
       const response = await fetch(endpoint);
       const forecastData = await response.json();
-      await this.cacheClient.addItem(`${cityName}:current`, forecastData);
       return Result.ok<GenericObject>(forecastData);
     } catch (err) {
-      return Result.fail(new ApplicationError(err.message, HttpStatusCodes.BAD_REQUEST));
+      return Result.fail(
+        new ApplicationError(err.message, HttpStatusCodes.BAD_REQUEST)
+      );
     }
   }
 }
--- src/services/WeatherService/index.ts
+++ src/services/WeatherService/index.ts
@@ -3,21 +3,60 @@ import { IOC_TYPES } from "@Common/constants";
 import { IWeatherClient } from "@Lib/WeatherClient";
 import { inject, injectable } from "inversify";
 import { IWeatherService } from "./IWeatherService";
+import { ICacheClient } from "@Lib/CacheClient";
+import { Result } from "@Common/logic";
+import { GenericObject } from "@Common/types";
 
 @injectable()
 export class WeatherService implements IWeatherService {
   private weatherClient: IWeatherClient;
+  private cacheClient: ICacheClient;
 
-  constructor(@inject(IOC_TYPES.WeatherClient) weatherClient: IWeatherClient) {
+  constructor(
+    @inject(IOC_TYPES.WeatherClient) weatherClient: IWeatherClient,
+    @inject(IOC_TYPES.CacheClient) cacheClient: ICacheClient
+  ) {
     this.weatherClient = weatherClient;
+    this.cacheClient = cacheClient;
   }
   async getCurrentWeather(cityName: string) {
-    const result = this.weatherClient.getCurrentWeather(cityName);
+    /** First check if current weather data is in cache and resturn from cache */
+    const cachedDataResult = await this.cacheClient.getItem(
+      `${cityName}:current`
+    );
+
+    if (cachedDataResult.isSuccess) {
+      const data = cachedDataResult.getValue();
+      return Result.ok(data as GenericObject);
+    }
+
+    /** If not found in cache the make api call to openweather and add to cache and return data */
+    const result = await this.weatherClient.getCurrentWeather(cityName);
+    if (result.isSuccess) {
+      await this.cacheClient.addItem(`${cityName}:current`, result.getValue());
+    }
+
     return result;
   }
 
   async getFiveDaysWeatherForecast(cityName: string) {
-    const result = this.weatherClient.getFiveDaysWeatherForecast(cityName);
+    /** First check if 5 days weather forecast data is in cache and resturn from cache */
+    const cachedDataResult = await this.cacheClient.getItem(
+      `${cityName}:forecast`
+    );
+
+    if (cachedDataResult.isSuccess) {
+      const data = cachedDataResult.getValue();
+      return Result.ok<GenericObject>(data as GenericObject);
+    }
+
+    /** If not found in cache the make api call to openweather and add to cache and return data */
+    const result = await this.weatherClient.getFiveDaysWeatherForecast(
+      cityName
+    );
+    if (result.isSuccess) {
+      await this.cacheClient.addItem(`${cityName}:forecast`, result.getValue());
+    }
     return result;
   }
 }
