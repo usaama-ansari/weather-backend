c7d0ba7c0fbdee3267b7b2bc3aee428c9d8d5d96
usaama ansari <usaama.ansari@massiveanalytic.com>
2023-03-27 17:44:57 +0530

    add environment script

---
+++ env_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DB_HOST=mongodb://localhost:27018/weather-forecast
+export WEATHER_API_KEY=858f15fed9292cbe25c341a754c55e45
+export WEATHER_API_ENDPOINT=https://api.openweathermap.org
+export NODE_ENV=development
